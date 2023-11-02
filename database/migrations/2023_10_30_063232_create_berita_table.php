7409e03adba1ad0d1be2cf03846d644e53a2ea23
BagusFary <bagusfaryananta@gmail.com>
2023-11-02 14:35:00 +0700

    Update

--- database/migrations/2023_10_30_063232_create_berita_table.php
+++ database/migrations/2023_10_30_063232_create_berita_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('berita', function (Blueprint $table) {
             $table->id();
-            $table->string('jusul', 100);
+            $table->string('judul', 100);
             $table->string('deskripsi', 100);
+            $table->string('gambar', 100);
             $table->timestamps();
         });
     }
